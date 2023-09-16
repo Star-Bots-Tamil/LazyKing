80ca8df9abde1206b6cd6fb1792ace4885d3778b
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-09-17 02:03:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -93,12 +93,12 @@ if 'DYNO' in environ:
     APP_NAME = environ.get('APP_NAME')
 else:
     ON_HEROKU = False
-BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', 'star-bots-stream.onrender.com'))
+BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', 'frail-waders-bat.cyclic.cloud'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
 URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else \
     "http://{}:{}/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
-WORKERS = int(environ.get('WORKERS', '4'))
+WORKERS = int(environ.get('WORKERS', '100'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
 MULTI_CLIENT = False
 HASH_LENGTH = int(environ.get("HASH_LENGTH", 7))
