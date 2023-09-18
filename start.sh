cfac7c725a52ed5cec9e917686ac67293d4665db
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-09-18 06:36:20 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Star-Bots-Tamil/LazyPrincess.git /LazyPrincess
+  git clone https://github.com/Star-Bots-Tamil/LazyPrincess.git /star-movies-tamil-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LazyPrincess
+  git clone $UPSTREAM_REPO /star-movies-tamil-bot
 fi
-cd /LazyPrincess
+cd /star-movies-tamil-bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
